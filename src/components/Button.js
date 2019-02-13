75fddb5596e966527c5490d657e244736cbcc0f8
A Gautreaux <armando.gautreaux@icloud.com>
2019-02-12 21:39:10 -0600

    Getting context using default values

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Button extends React.Component {
+  static contextType = LanguageContext;
+  render() {
+    console.log(this.context);
+    return <div className="ui button primary">Submit</div>;
+  }
+}
+
+export default Button;
