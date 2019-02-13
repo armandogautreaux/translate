92bab9a45b07305711505f3d18cf64f3d46c257b
A Gautreaux <armando.gautreaux@icloud.com>
2019-02-12 22:25:35 -0600

    Added Provider and Consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
+  // static contextType = LanguageContext;
+  // render() {
+  //   const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+  //   return <div className="ui button primary">{text}</div>;
+  // }
   render() {
-    console.log(this.context);
-    return <div className="ui button primary">Submit</div>;
+    return (
+      <ColorContext.Consumer>
+        {color => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {value => (value === 'english' ? 'Submit' : 'Voorleggen')}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
+    );
   }
 }
 
 export default Button;
+
+//Use .consumer when trying to get info from multiple different context objects inside of a single component
+//.contenxt when use a single context object inside of a components
